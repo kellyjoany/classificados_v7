08b671606563bc59d4cab9983d5714a6d4f5e690
kellyjoany <45659063+kellyjoany@users.noreply.github.com>
2020-02-06 20:01:36 -0300

    versao_v7

---
+++ projeto_classificados_15/index.php
@@ -0,0 +1,28 @@
+<?php require 'pages/header.php' ?>
+        
+<?php
+    require 'classes/anuncios.class.php';
+    require 'classes/usuarios.class.php';
+    $a = new Anuncios();
+    $u = new Usuarios();
+
+    $total_anuncios = $a->getTotalAnuncios();
+    $total_usuarios = $u->getTotalUsuarios();                  
+?>        
+        <div class="container-fluid">
+            <div class="jumbotron">
+                <h2>Nós temos <?php echo $total_anuncios; ?> anúncios.</h2>
+                <p>E mais de <?php echo $total_usuarios; ?> usuários cadastrados.</p>
+            </div>
+
+            <div class="row">
+                <div class="col-sm-3">
+                  <h4>Pesquisa avançada</h4>
+                </div>
+                <div class="col-sm-9">   
+                  <h4>Últimos anúncios</h4>  
+                </div>              
+            </div>
+        </div>
+
+<?php require 'pages/footer.php' ?>
